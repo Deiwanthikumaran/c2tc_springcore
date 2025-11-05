dd5e82ddb20ec67d5932eab1351b8ae2ca2c3637
deiwanthi <deiwanthi@deiwa>
2025-11-05 12:34:23 +0530

    commit

---
+++ springannotations/src/main/java/org/tnsif/springdi/springannotations/application/ApplicationConfig.java
@@ -0,0 +1,7 @@
+package org.tnsif.springdi.springannotations.application;
+
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan({"com.springannotations"})
+public class ApplicationConfig {
+
+}
